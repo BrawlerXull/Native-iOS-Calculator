fb446c59a877300530875801b0207fe8d748b74d
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2023-04-13 23:23:50 +0530

    Functionality added with basic UI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,32 +7,47 @@
 
 import SwiftUI
 import UIKit
-
+import MathParser
 struct ContentView: View {
-    @State private var username: String = ""  ;
+    @State private var input: String = ""  ;
+    @State var result:String = "";
+//    let parser = MathParser()
     var body: some View {
         VStack {
-            TextField("THIS IS THE TEXT FIELD",text: $username)
+            TextField("THIS IS THE TEXT FIELD",text: $input)
+//            Text("Hello, \(input)!")
             HStack{
                 Button("1") {
+                    input = input + "1"
                     print("1 pressed")
+                    print(input)
                 }
                 .padding(40.0)
                 .background(Color.gray)
 
                 Button("2") {
+                    input = input + "2"
                     print("2 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
 
                 Button("3") {
+                    input = input + "3"
                     print("3 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
                 Button("=") {
-                    print("3 pressed")
+                    let parser = MathParser()
+                    let evaluator = parser.parse(input)
+                    if let evaluator = evaluator {
+                        print(evaluator.eval())
+                        result = "\(evaluator.eval())"
+                        input=result
+                    }
+                    print("= pressed")
+                    
                 }
                 .padding(40.0)
                 .background(Color.gray)
@@ -41,23 +56,27 @@ struct ContentView: View {
 
             HStack{
                 Button("4") {
+                    input = input + "4"
                     print("4 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
 
                 Button("5") {
+                    input = input + "5"
                     print("5 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
 
                 Button("6") {
+                    input = input + "6"
                     print("6 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
                 Button("+") {
+                    input = input + "+"
                     print("3 pressed")
                 }
                 .padding(40.0)
@@ -66,23 +85,27 @@ struct ContentView: View {
 
             HStack{
                 Button("7") {
+                    input = input + "7"
                     print("7 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
 
                 Button("8") {
+                    input = input + "8"
                     print("8 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
 
                 Button("9") {
+                    input = input + "9"
                     print("9 pressed")
                 }
                 .padding(40.0)
                 .background(Color.gray)
                 Button("-") {
+                    input = input + "-"
                     print("- pressed")
                 }
                 .padding(40.0)
@@ -90,6 +113,7 @@ struct ContentView: View {
             }
             HStack{
                 Button("0") {
+                    input = input + "0"
                     print("0 pressed")
                 }
                 .padding(.vertical, 40 )
@@ -98,6 +122,7 @@ struct ContentView: View {
                 .background(Color.gray)
 
                 Button("/") {
+                    input = input + "/"
                     print("9 pressed")
                 }
                 .padding(40.0)
